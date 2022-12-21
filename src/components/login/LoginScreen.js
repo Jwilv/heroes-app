dd245a36657a21d4a6d94d2e3655277e48df136d
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-21 15:50:19 -0300

    button en login

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Navigate, redirect, Route } from 'react-router'
+import { Link } from 'react-router-dom'
 
 export const LoginScreen = () => {
+    const handleLogin = () => {
+        
+    }
     return (
-        <div>
-            <h1>Login Screen</h1>
+        <div className='container mt-5'>
+            <h1>Login</h1>
+            <hr />
+            <button className='btn btn-primary'
+                onClick={handleLogin}
+            >
+                Login
+                {false ? <Link to={'/'}>pasa</Link> : <Link>no podes pasar</Link>}
+            </button>
         </div>
     )
 }
