45040106d8e83543c47fe9072e8cd906bf243f51
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-21 01:06:44 -0300

    se agrego la ruta del error 404 y se modifico la ruta inicial

--- src/components/routers/DashboardRouter.js
+++ src/components/routers/DashboardRouter.js
@@ -14,10 +14,11 @@ export const DashboardRouter = () => {
         <>
             <Navbar />
             <Routes>
-                <Route  path='/marvel' element={<MarverlScreen />} />
-                <Route  path='/heroe/:heroeId' element={<HeroesScreen />} />
-                <Route  path='/dc' element={<DcScreen />} />
-                <Route path='*' element={<DcScreen />}/>
+                <Route path='/' element={<HeroesScreen />} />
+                <Route path='/marvel' element={<MarverlScreen />} />
+                <Route path='/heroe/:heroeId' element={<HeroesScreen />} />
+                <Route path='/dc' element={<DcScreen />} />
+                <Route path='*' element={<h1>404</h1>} /> 
             </Routes>
         </>
     )
