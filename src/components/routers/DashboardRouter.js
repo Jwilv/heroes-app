cee0c50e181487ebe031c81dead1da3e0624d644
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-22 16:06:35 -0300

    se agregaron estilos en el DashboardRouter para separar el contenido del NavBar

--- src/components/routers/DashboardRouter.js
+++ src/components/routers/DashboardRouter.js
@@ -13,13 +13,15 @@ export const DashboardRouter = () => {
     return (
         <>
             <Navbar />
-            <Routes>
-                <Route path='/' element={<HeroesScreen />} />
-                <Route path='/marvel' element={<MarverlScreen />} />
-                <Route path='/heroe/:heroeId' element={<HeroesScreen />} />
-                <Route path='/dc' element={<DcScreen />} />
-                <Route path='*' element={<h1>404 no page</h1>} /> 
-            </Routes>
+            <div className='container mt-2'>
+                <Routes>
+                    <Route path='/' element={<HeroesScreen />} />
+                    <Route path='/marvel' element={<MarverlScreen />} />
+                    <Route path='/heroe/:heroeId' element={<HeroesScreen />} />
+                    <Route path='/dc' element={<DcScreen />} />
+                    <Route path='*' element={<h1>404 no page</h1>} />
+                </Routes>
+            </div>
         </>
     )
 }
