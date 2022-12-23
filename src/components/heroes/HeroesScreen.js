c02325f9eb5cbe0af0e5993d91a605e62cd277b3
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-23 18:51:59 -0300

    se agrego un boton para regresar usando useNavigate con el valor de PREVIOUS_PAGE, se agrego la img de el hero, se agregaron una lista de elementos del hero, se agrego los characters

--- src/components/heroes/HeroesScreen.js
+++ src/components/heroes/HeroesScreen.js
@@ -1,11 +1,22 @@
 import React from 'react'
-import { Navigate, useParams } from 'react-router'
+import { Navigate, useNavigate, useParams } from 'react-router'
 import { getHeroesById } from '../selectors/getHeroeById';
 
 export const HeroesScreen = () => {
-    const {heroId} = useParams();
+    const { heroId } = useParams();
+
+    const navigate = useNavigate();
+
     const hero = getHeroesById(heroId);
-    if(!hero) return <Navigate to={'/marvel'}/>
+
+    if (!hero) return <Navigate to={'/marvel'} />
+
+    const PREVIOUS_PAGE = -1;
+
+    const handleRegresar = () => {
+        navigate( PREVIOUS_PAGE );
+    }
+
     const {
         superhero,
         publisher,
@@ -13,10 +24,35 @@ export const HeroesScreen = () => {
         first_appearance,
         characters,
     } = hero
-    return (
-        <div>
-            <h1>HeroesScreen</h1>
 
+    return (
+        <div className='row mt-5'>
+            <div className='col-4'>
+                <img
+                    src={`../assets/heroes/${heroId}.jpg`}
+                    alt={superhero}
+                    className='img-thumbnail'
+                />
+            </div>
+            <div className='col-8'>
+                <h2>{superhero}</h2>
+                <ul className='list-group list-group-flush'>
+                    <li className='list-group-item'> <b> Alter ego: </b>{alter_ego} </li>
+                    <li className='list-group-item'> <b> Publisher: </b>{publisher} </li>
+                    <li className='list-group-item'> <b> First appearance: </b>{first_appearance} </li>
+                </ul>
+                <div className='m-2'>
+                    <h5 className='m-1'>Characters</h5>
+                    <p className='m-1'>{characters}</p>
+                </div>
+
+                <button
+                    className='btn btn-outline-info'
+                    onClick={handleRegresar}
+                >
+                    Regresar
+                </button>
+            </div>
         </div>
     )
 }
