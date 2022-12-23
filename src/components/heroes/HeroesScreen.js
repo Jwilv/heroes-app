b18f76d485f8d4c3cf81074683ac65ecc57704ed
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-23 18:15:13 -0300

    se agrego Navigate para redireccionar al user si el hero no es valido o no existe, caso contraeio se obtienen las propiedades de este

--- src/components/heroes/HeroesScreen.js
+++ src/components/heroes/HeroesScreen.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import { useParams } from 'react-router'
+import { Navigate, useParams } from 'react-router'
+import { getHeroesById } from '../selectors/getHeroeById';
 
 export const HeroesScreen = () => {
     const {heroId} = useParams();
+    const hero = getHeroesById(heroId);
+    if(!hero) return <Navigate to={'/marvel'}/>
+    const {
+        superhero,
+        publisher,
+        alter_ego,
+        first_appearance,
+        characters,
+    } = hero
     return (
         <div>
             <h1>HeroesScreen</h1>
-            <h1>{heroId}</h1>
 
         </div>
     )
