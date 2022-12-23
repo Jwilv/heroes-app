aed2760702710af91d5b7f76d6032aae74ff9099
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-23 19:05:11 -0300

    se agrego useMemo en HeroesList

--- src/components/heroes/HeroesList.js
+++ src/components/heroes/HeroesList.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, {useMemo} from 'react'
 import { getHeroesByPublisher } from '../selectors/getHeroesByPublisher'
 import { HeroesCard } from './HeroesCard'
 
 export const HeroesList = ({ publisher }) => {
 
-  const heroes = getHeroesByPublisher(publisher);
+  const heroes = useMemo( ()=> getHeroesByPublisher(publisher), [publisher])
+  
   return (
     <div className='card-columns'>
       <div className='row'>
