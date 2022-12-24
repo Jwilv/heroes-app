a9b0c8994b7f2ae49c353e5b22c495b5f51dd700
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-24 18:18:29 -0300

    se utilizo el context y el reducer para el user en HeroesApp

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+import { AuthContext } from './auth/AuthContext'
+import { authReducer } from './auth/authReducer'
 import { AppRouter } from './routers/AppRouter'
 
+//init va a darnos el estado de inicio, en este caso si existe el user
+//en el localStorage lo va a retornar, en cambio si no existe
+//va a retornar un objeto con el logged en false indicando que el usuario no esta logeado
+const init = ()=>{
+  return JSON.parse(localStorage.getItem('user')) || { logged:false }
+}
+
+
 export const HeroesApp = () => {
+  const [user, dispatch] = useReducer(authReducer, {}, init)
   return (
-    <AppRouter />
+    <AuthContext.Provider value={{ user, dispatch}}>
+      <AppRouter />
+    </AuthContext.Provider>
   )
 }
