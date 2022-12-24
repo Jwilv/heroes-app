41a8e0c83e650c7055e0cd7f4e6cbe9814c15552
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-24 19:07:04 -0300

    se agrego el user al localStorage

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react'
+import React, { useEffect, useReducer } from 'react'
 import { AuthContext } from './auth/AuthContext'
 import { authReducer } from './auth/authReducer'
 import { AppRouter } from './routers/AppRouter'
@@ -11,8 +11,12 @@ const init = ()=>{
 }
 
 
+
 export const HeroesApp = () => {
-  const [user, dispatch] = useReducer(authReducer, {}, init)
+  const [user, dispatch] = useReducer(authReducer, {}, init);
+  useEffect(() => {
+    localStorage.setItem('user', JSON.stringify(user))
+  }, [user])
   return (
     <AuthContext.Provider value={{ user, dispatch}}>
       <AppRouter />
