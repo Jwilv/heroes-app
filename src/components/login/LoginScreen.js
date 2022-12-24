77e8d8537cef98615ff7d90c03086b88ade339c9
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-24 18:39:37 -0300

    se agrego el useContext para establever un user cuando el usuario se logea

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useNavigate } from 'react-router'
+import { AuthContext } from '../../auth/AuthContext';
+import { types } from '../../type/types';
 
 
 export const LoginScreen = () => {
-const navigate = useNavigate();
+    const navigate = useNavigate();
+    const { dispatch } = useContext(AuthContext)
     const handleLogin = () => {
         navigate('/',{replace: true})
+        dispatch({
+            type: types.login,
+            payload: { name: 'juan' },
+        })
     }
     return (
         <div className='container mt-5'>
