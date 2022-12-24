aac5d62c9113bf2a225ef0ed3cca54a4e3e0dec7
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-24 18:45:11 -0300

    se agrego el nombre del user en Navbar

--- src/components/ui/NavBar.js
+++ src/components/ui/NavBar.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import { AuthContext } from '../../auth/AuthContext';
 
 
 export const Navbar = () => {
+
+    const { user } = useContext(AuthContext)
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
             
@@ -41,6 +44,7 @@ export const Navbar = () => {
 
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
                 <ul className="navbar-nav ml-auto">
+                    <span className='nav-item nav-link text-info'>{user.name}</span>
                     <NavLink 
                         className="nav-item nav-link" 
                         to="/login"
