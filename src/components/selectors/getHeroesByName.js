58b21bbe01307107029cf8d4119f889467a4a278
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-24 15:04:14 -0300

    se arreglo bug

--- src/components/selectors/getHeroesByName.js
+++ src/components/selectors/getHeroesByName.js
@@ -3,7 +3,7 @@ import { heroes } from "../data/heroes";
 export const getHeroesByName = (name = '') => {
     if( name === '') return [];
     name = name.toLocaleLowerCase();
-    heroes.filter( hero => hero.superhero.toLocaleLowerCase().includes(name) )
+    return heroes.filter( hero => hero.superhero.toLocaleLowerCase().includes(name) )
 }
 
 // toLocaleLowerCase para pasar tod a minusculas 
