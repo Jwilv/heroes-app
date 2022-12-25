6ca8a26b46bba60776749db087ca923bdde0096d
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-25 20:10:23 -0300

    se agrego nombre de usuario en el login

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,28 +1,49 @@
 import React, { useContext } from 'react'
 import { useNavigate } from 'react-router'
 import { AuthContext } from '../../auth/AuthContext';
+import { useForm } from '../../hooks/useForm';
 import { types } from '../../type/types';
 
 
 export const LoginScreen = () => {
     const navigate = useNavigate();
+
     const { dispatch } = useContext(AuthContext)
-    const handleLogin = () => {
-        navigate('/',{replace: true})
+
+    const [ { userName }, handleInputChanGet ] = useForm({ userName: '' }) ;
+
+    const handleLogin = (event) => {
+
+        event.preventDefault();
+
+        navigate('/', { replace: true })
+
         dispatch({
             type: types.login,
-            payload: { name: 'juan' },
+            payload: { name: userName },
         })
     }
     return (
         <div className='container mt-5'>
             <h1>Login</h1>
             <hr />
-            <button className='btn btn-primary'
-                onClick={handleLogin}
-            >
-                Login
-            </button>
-        </div>
+            <form onSubmit={handleLogin}>
+                <input
+                    type={'text'}
+                    autoComplete='off'
+                    placeholder={'Nombre de usuario'}
+                    className={'form-control'}
+                    name={'userName'}
+                    value={userName}
+                    onChange={handleInputChanGet}
+                />
+                <button
+                    type='submit'
+                    className='btn btn-primary'
+                >
+                    Login
+                </button>
+            </form>
+        </div >
     )
 }
