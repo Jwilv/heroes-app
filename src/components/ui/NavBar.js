7d10ba46e0955811efa55b66dbacb55190e8ef39
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-25 17:24:22 -0300

    se remplazo el navlink por un boton con la funcionalidad del handleLogout y el dispacht

--- src/components/ui/NavBar.js
+++ src/components/ui/NavBar.js
@@ -1,19 +1,33 @@
 import React, { useContext } from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../auth/AuthContext';
+import { types } from '../../type/types';
 
 
 export const Navbar = () => {
-//esto sirve para desestrcturar una propiedo de un obejeto ( user )
-// y cons :{} al aldo podemos desestructurar la propiedad que queremos del objeto que ya desestructuramos
-// ej const { user:{name} ( aca sacamos la propiedad del objeto que desestructuramos ) } = useContext(AuthContext)
-//es una segunda desestructuracion que se puede hacer dentro de la primera 
-    const { user:{name} } = useContext(AuthContext)
+    //esto sirve para desestrcturar una propiedo de un obejeto ( user )
+    // y cons :{} al aldo podemos desestructurar la propiedad que queremos del objeto que ya desestructuramos
+    // ej const { user:{name} ( aca sacamos la propiedad del objeto que desestructuramos ) } = useContext(AuthContext)
+    //es una segunda desestructuracion que se puede hacer dentro de la primera 
+    const { user: { name }, dispatch } = useContext(AuthContext);
+
+    const navegate = useNavigate();
+
+    const handleLogout = () => {
+
+        dispatch({
+            type:types.logout,
+        });
+
+        navegate('/login', {replace : true});
+    }
+
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-            
-            <Link 
-                className="navbar-brand" 
+
+            <Link
+                className="navbar-brand"
                 to="/"
             >
                 Asociaciones
@@ -22,22 +36,22 @@ export const Navbar = () => {
             <div className="navbar-collapse">
                 <div className="navbar-nav">
 
-                    <NavLink 
-                        className="nav-item nav-link" 
+                    <NavLink
+                        className="nav-item nav-link"
                         to="/marvel"
                     >
                         Marvel
                     </NavLink>
 
-                    <NavLink 
-                        className="nav-item nav-link" 
+                    <NavLink
+                        className="nav-item nav-link"
                         to="/dc"
                     >
                         DC
                     </NavLink>
 
-                    <NavLink 
-                        className="nav-item nav-link" 
+                    <NavLink
+                        className="nav-item nav-link"
                         to="/search"
                     >
                         Buscar
@@ -48,12 +62,12 @@ export const Navbar = () => {
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
                 <ul className="navbar-nav ml-auto">
                     <span className='nav-item nav-link text-info'>{name}</span>
-                    <NavLink 
-                        className="nav-item nav-link" 
-                        to="/login"
+                    <button
+                        className="nav-item nav-link btn"
+                        onClick={handleLogout}
                     >
                         Logout
-                    </NavLink>
+                    </button>
                 </ul>
             </div>
         </nav>
