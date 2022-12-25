38c961916f589e7a7e67e542df74a6faee461265
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-25 18:37:56 -0300

    se corrigio AppRouter

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -14,9 +14,6 @@ export const AppRouter = () => {
             <Routes>
                 <Route exact path='/login' element={<LoginScreen />} />
                 <Route  path='*' element={<DashboardRouter />} /> 
-                {/* <Route path='*' element={<h1>404 alla la ponen en 4</h1>} /> */}
-
-
             </Routes>
         </>
     )
