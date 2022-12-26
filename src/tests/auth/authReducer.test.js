01580f06af76d77f598c05a495d84a48aceafbe3
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-26 17:39:00 -0300

    debe de autenticar y colocar el name del user

--- src/tests/auth/authReducer.test.js
+++ src/tests/auth/authReducer.test.js
@@ -1,4 +1,5 @@
 import { authReducer } from "../../auth/authReducer";
+import { types } from "../../type/types";
 import { demoAuthTodo } from "../fixtures/demoAuthTodo";
 
 describe('pruebas en authReducer', () => {
@@ -7,4 +8,13 @@ describe('pruebas en authReducer', () => {
         const state = authReducer(demoAuthTodo, {});
         expect(state).toEqual(demoAuthTodo);
     })
+
+    test('debe de autenticar y colocar el name del user', () => {
+        const state = authReducer( {} , {
+            type: types.login,
+            payload:demoAuthTodo[0]
+        });
+        expect(state.user).toEqual(demoAuthTodo[0].user);
+    })
+    
 })
