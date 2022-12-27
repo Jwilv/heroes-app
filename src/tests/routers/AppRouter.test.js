f3d455c5aaee4abc757ca4dced1f0f912e61f6be
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-27 14:59:36 -0300

    se creo test para  AppRouter

---
+++ src/tests/routers/AppRouter.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+describe('pruebas en <AppRouter />', () => {
+
+    test('debe de mostrar el login si no esta autenticado', () => {
+
+
+    })
+})
